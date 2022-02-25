067f11d3077c1e5f8128b5666779e641e127533b
sakdt <sakdt5@web.de>
2022-02-25 14:36:18 +0100

    first

---
+++ index.js
@@ -0,0 +1,15 @@
+//les fonctions
+
+(function () {
+  console.log("Hello Steve!");
+})();
+
+const gruess = (name) => {
+  console.log("Hello " + name);
+};
+
+gruess("Steve !");
+
+function calc(x, y) {
+  return x + y;
+}
